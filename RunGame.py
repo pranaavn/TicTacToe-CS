e38c6a9639b54aef6f964a0eb29cb15dfecb9a0d
pranaavn <53028357+pranaavn@users.noreply.github.com>
2019-07-22 20:01:47 -0700

    Update RunGame.py

--- RunGame.py
+++ RunGame.py
@@ -4,13 +4,27 @@ import time
 from cs043_Final_Project.TicTacToe import ticTacToe
 
 game = ticTacToe()
+
 print('Welcome to Tic Tac Toe!')
+print('Do you already know how to play? (Yes or No)')
+response = input().lower()
+
+if response == 'no':
+    game.howToPlay()
+else:
+    print('Alright, let\'s begin!')
+
+print('Please choose who will be player 1 and who will be player 2.')
+time.sleep(1)
+
 
 while True:
     # Reset the board
     theBoard = [' '] * 10
     playerLetter, computerLetter = game.inputPlayerLetter()
     turn = game.whoGoesFirst()
+    print('Randomly choosing who will go first...')
+    time.sleep(2)
     print( turn + ' will go first.')
     time.sleep(1.5)
     gameIsPlaying = True
